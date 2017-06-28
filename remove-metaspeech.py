fd17e8e0616d526acf351d8282e15183072bf270
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-06-28 12:51:50 +0100

    Get rid of leading/trailing quotes and whitespace

--- remove-metaspeech.py
+++ remove-metaspeech.py
@@ -49,7 +49,8 @@ def main():
                         prints("    Skipping suspected stage direction: {line}".format(line=line.strip(" \n")))
                         continue
                     else:
-                        target_file.write(line)
+                        # strip of leading/trailing punctuation and whitespace
+                        target_file.write(line.strip('\'\" \n-'))
 
         prints("{file_i:05d}: Processed {file_name}".format(file_i=count, file_name=target_path))
 
