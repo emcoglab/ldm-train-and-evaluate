876e75b8c7fbc1e1bf1a8307affd6e253a764641
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-17 16:52:52 +0100

    Wow... you had one job...

--- scripts_common/0 Combine corpora.sh
+++ scripts_common/0 Combine corpora.sh
@@ -15,23 +15,23 @@ echo "Copying speech documents"
 echo "========"
 echo "Copying BBC subtitles"
 for f in "$bbc_dir"/* ; do
-    cp -v "$f" "$combined_speech_dir"
+    cp "$f" "$combined_speech_dir"
 done
 echo "--------"
 echo "Copying BNC speech"
 for f in "$bnc_speech_dir"/* ; do
-    cp -v "$f" "$combined_speech_dir"
+    cp "$f" "$combined_speech_dir"
 done
 echo "--------"
 echo "Copying BNC text-for-speech"
 for f in "$bnc_text_for_speech_dir"/* ; do
-    cp -v "$f" "$combined_speech_dir"
+    cp "$f" "$combined_speech_dir"
 done
 
 echo "========"
 echo "Copying text documents"
 echo "========"
 echo "Copying BNC text"
-for f in "$bnc_text_for_speech_dir"/* ; do
-    cp -v "$f" "$combined_text_dir"
+for f in "$bnc_text_dir"/* ; do
+    cp "$f" "$combined_text_dir"
 done
