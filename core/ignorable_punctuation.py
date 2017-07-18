c62ccb7af97f51c36ba1434a498f97b8e2926fde
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-18 14:00:27 +0100

    We can ignore fancy single quotes

--- core/ignorable_punctuation.py
+++ core/ignorable_punctuation.py
@@ -10,4 +10,6 @@
 #  - we do want to ignore
 #       \u2026 …
 #       \u2013 –
-ignorable_punctuation = r"""!"#'()*,-./:;<>?[\]^_`{|}~\u2026\u2013"""
+#       \u2018 ‘
+#       \u2019 ’
+ignorable_punctuation = r"""!"#'()*,-./:;<>?[\]^_`{|}~\u2026\u2013\u2018\u2019"""
