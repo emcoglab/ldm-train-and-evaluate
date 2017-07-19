449fa0ab70fbae218d6fa6f64478ecf72e6229f4
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-19 16:08:53 +0100

    Do these seriously have to be in the other order.

--- core/classes.py
+++ core/classes.py
@@ -1,3 +1,14 @@
+class CorpusMetaData:
+    """
+    Corpus metadata
+    """
+
+    def __init__(self, name, path, info_path=None):
+        self.name = name
+        self.path = path
+        self.info_path = info_path
+
+
 class SourceTargetPair:
     """
     A pair of a source directory and a target directory.
@@ -6,13 +17,3 @@ class SourceTargetPair:
     def __init__(self, source: CorpusMetaData, target: CorpusMetaData):
         self.source = source
         self.target = target
-
-
-class CorpusMetaData:
-    """
-    Corpus metadata
-    """
-
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
