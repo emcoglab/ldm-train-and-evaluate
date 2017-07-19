1ce3b8c4fa3f5e13e00e304762248811cff68253
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-19 16:22:02 +0100

    Simpler Brysbaert vocab checker.

---
+++ scripts_corpus_info/brysbaert_vocab_overlap.py
@@ -0,0 +1,53 @@
+import logging
+import os
+import sys
+
+from nltk.corpus import PlaintextCorpusReader
+
+from ..core.classes import CorpusMetaData
+
+logger = logging.getLogger()
+
+
+def main():
+
+    corpus_metas = [
+        CorpusMetaData(
+            name="BBC", path="/Users/caiwingfield/corpora/BBC/4 Tokenised"),
+        CorpusMetaData(
+            name="BNC", path="/Users/caiwingfield/corpora/BNC/2 Tokenised"),
+    ]
+
+    logger.info(f"Loading wordlist from {wordlist_path}")
+    wordlist = PlaintextCorpusReader("/Users/caiwingfield/corpora/brysbaert40k/", "brysbaert1.txt").raw().split("\n")
+    vocab_wordlist = set(wordlist)
+
+    logger.info(f"Wordlist has a vocab of size {len(vocab_wordlist):,}")
+
+    for corpus_meta in corpus_metas:
+
+        logger.info(f"Loading {corpus_meta.name} corpus documents from {corpus_meta.path}")
+        corpus_dir, corpus_filename = os.path.split(corpus_meta.path)
+        corpus = PlaintextCorpusReader(corpus_dir, corpus_filename).raw().split("\n")
+        vocab_corpus = set(corpus)
+        logger.info(f"Corpus has a vocab of size {len(vocab_corpus):,}")
+
+        overlap_vocab = set.intersection(vocab_corpus, vocab_wordlist)
+        logger.info(f"Overlap with cutoff freq {cutoff_freq} has a size of\t{len(overlap_vocab):,}")
+
+        missing_vocab = vocab_wordlist - vocab_corpus
+        logger.info(f"\tMissing words: {missing_vocab}")
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        format='%(asctime)s | %(levelname)s | %(module)s | %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO)
+    logger.info("Running %s" % " ".join(sys.argv))
+    logger.info("")
+
+    main()
+
+    logger.info("Done!")
