acbf4d162e544c3e5cc8dd6c511be5132844be07
Cai <mail@caiwingfield.com>
2017-07-21 10:06:50 +0100

    Don't need a PlaintextCorpusReader to read a \n-delimited file.

--- scripts_corpus_info/brysbaert_vocab_overlap.py
+++ scripts_corpus_info/brysbaert_vocab_overlap.py
@@ -1,8 +1,5 @@
 import logging
-import os
 import sys
-
-from nltk.corpus import PlaintextCorpusReader
 
 from ..core.classes import CorpusMetaData
 
@@ -13,23 +10,22 @@ def main():
 
     corpus_metas = [
         CorpusMetaData(
-            name="BBC", path="/Users/caiwingfield/corpora/BBC/4 Tokenised/BBC.corpus"),
+            name="BBC", path="/Users/cai/Desktop/BBC.corpus"),
         CorpusMetaData(
-            name="BNC", path="/Users/caiwingfield/corpora/BNC/2 Tokenised/BNC.corpus"),
+            name="BNC", path="/Users/cai/Desktop/BNC.corpus"),
     ]
 
-    logger.info(f"Loading wordlist from /Users/caiwingfield/corpora/brysbaert40k/brysbaert1.txt")
-    wordlist = PlaintextCorpusReader("/Users/caiwingfield/corpora/brysbaert40k/", "brysbaert1.txt").raw().split("\n")
-    vocab_wordlist = set(wordlist)
+    logger.info(f"Loading wordlist from /Users/cai/Desktop/b.txt")
+    with open("/Users/cai/Desktop/b.txt", mode="r") as wordlist_file:
+        vocab_wordlist = set(wordlist_file.read().split("\n"))
 
     logger.info(f"Wordlist has a vocab of size {len(vocab_wordlist):,}")
 
     for corpus_meta in corpus_metas:
 
         logger.info(f"Loading {corpus_meta.name} corpus documents from {corpus_meta.path}")
-        corpus_dir, corpus_filename = os.path.split(corpus_meta.path)
-        corpus = PlaintextCorpusReader(corpus_dir, corpus_filename).raw().split("\n")
-        vocab_corpus = set(corpus)
+        with open(corpus_meta.path, mode="r") as corpus_file:
+            vocab_corpus = set(corpus_file.read().split("\n"))
         logger.info(f"Corpus has a vocab of size {len(vocab_corpus):,}")
 
         overlap_vocab = set.intersection(vocab_corpus, vocab_wordlist)
@@ -45,8 +41,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         format='%(asctime)s | %(levelname)s | %(module)s | %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-        filename="/Users/caiwingfield/Dropbox/brysbaert.log")
+        level=logging.INFO)
     logger.info("Running %s" % " ".join(sys.argv))
     logger.info("")
 
