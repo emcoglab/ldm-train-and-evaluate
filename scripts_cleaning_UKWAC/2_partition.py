cf1740280d5b21329f847e43cd388be5301b2691
Cai <mail@caiwingfield.com>
2017-07-24 15:15:12 +0100

    Partition corpus for tokenisation. (nltk.corpus.PlaintextCorpusReader doesn't deal well with large files).

---
+++ scripts_cleaning_UKWAC/2_partition.py
@@ -0,0 +1,59 @@
+import logging
+import os
+import sys
+
+from ..core.classes import SourceTargetPair, CorpusMetaData
+
+logger = logging.getLogger()
+
+
+def main():
+
+    part_size_lines = 10_000
+    target_filename_pattern = "part_{}.txt"
+
+    corpus_meta = SourceTargetPair(
+        source=CorpusMetaData(
+            name="UKWAC",
+            path="/Users/cai/Dox/Academic/Analyses/Corpus analysis/UKWAC/1 Text only/cleaned_pre.pos.corpus"),
+        target=CorpusMetaData(
+            name="UKWAC",
+            path="/Users/cai/Dox/Academic/Analyses/Corpus analysis/UKWAC/2 Partitioned"))
+
+    logger.info(f"Loading {corpus_meta.source.name} corpus from {corpus_meta.source.path}")
+
+    with open(corpus_meta.source.path, mode="r", encoding="utf-8") as source_file:
+
+        # initialise some variables
+        part_number = 0
+        lines = []
+        total_line_count = 0
+
+        while True:
+            line = source_file.readline()
+            if not line:
+                break
+
+            lines.append(line)
+
+            if len(lines) >= part_size_lines:
+                part_number += 1
+                target_path = os.path.join(corpus_meta.target.path, target_filename_pattern.format(part_number))
+
+                total_line_count += len(lines)
+
+                logger.info(f"Writing next {len(lines):,} lines to {os.path.basename(target_path)}. ({total_line_count:,} lines total.)")
+
+                with open(target_path, mode="w", encoding="utf-8") as target_file:
+                    target_file.writelines(lines)
+
+                # empty lines
+                lines = []
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.INFO)
+    logger.info("Running %s" % " ".join(sys.argv))
+    main()
+    logger.info("Done!")
