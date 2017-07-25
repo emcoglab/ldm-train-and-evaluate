b3ac89d231baf253f33c84022e53ef33a2c74d2b
Cai <mail@caiwingfield.com>
2017-07-25 10:33:58 +0100

    Save brysbaert info to file named in args.

--- scripts_corpus_info/brysbaert_vocab_overlap.py
+++ scripts_corpus_info/brysbaert_vocab_overlap.py
@@ -1,4 +1,6 @@
+import argparse
 import logging
+import os
 import sys
 
 from ..core.classes import CorpusMetaData
@@ -6,48 +8,57 @@ from ..core.classes import CorpusMetaData
 logger = logging.getLogger()
 
 
-def main():
+def main(corpus_path, output_dir):
+    wordlist_meta = CorpusMetaData(
+        name="Brysbaert 1 word", path="/Users/caiwingfield/corpora/brysbaert40k/brysbaert1.wordlist")
 
-    corpus_metas = [
-        CorpusMetaData(
-            name="BBC", path="/Users/caiwingfield/corpora/BBC/4 Tokenised/BBC.corpus"),
-        CorpusMetaData(
-            name="BNC", path="/Users/caiwingfield/corpora/BNC/2 Tokenised/BNC.corpus"),
-    ]
-    wordlist = CorpusMetaData(
-        name="Brysbaert 1 word", path="/Users/caiwingfield/corpora/brysbaert40k/brysbaert1.wordlist"
-    )
+    info_filename = "Brysbaert 1-word vocab overlap.info"
 
-    logger.info(f"Loading wordlist from {wordlist.path}")
-    with open(wordlist.path, mode="r") as wordlist_file:
+    logger.info(f"Loading wordlist from {wordlist_meta.path}")
+    with open(wordlist_meta.path, mode="r") as wordlist_file:
         vocab_wordlist = set(wordlist_file.read().split("\n"))
 
     logger.info(f"Wordlist has a vocab of size {len(vocab_wordlist):,}")
 
-    for corpus_meta in corpus_metas:
+    logger.info(f"Loading corpus documents from {corpus_path}")
+    with open(corpus_path, mode="r") as corpus_file:
+        vocab_corpus = set(corpus_file.read().split("\n"))
 
-        logger.info(f"Loading {corpus_meta.name} corpus documents from {corpus_meta.path}")
-        with open(corpus_meta.path, mode="r") as corpus_file:
-            vocab_corpus = set(corpus_file.read().split("\n"))
-        logger.info(f"Corpus has a vocab of size {len(vocab_corpus):,}")
+    with open(os.path.join(output_dir, info_filename), mode="w", encoding="utf-8") as info_file:
+        message = f"Corpus has a vocab of size {len(vocab_corpus):,}"
+        log_and_write(message, info_file)
 
         overlap_vocab = set.intersection(vocab_corpus, vocab_wordlist)
-        logger.info(f"Overlap has a size of\t{len(overlap_vocab):,}")
+
+        message = f"Overlap has a size of\t{len(overlap_vocab):,}"
+        log_and_write(message, info_file)
 
         missing_vocab = vocab_wordlist - vocab_corpus
-        logger.info(f"Missing words: {len(missing_vocab)}")
-        logger.info(f"{missing_vocab}")
+
+        message = f"Missing words: {len(missing_vocab)}"
+        log_and_write(message, info_file)
+
+        message = f"{missing_vocab}"
+        log_and_write(message, info_file)
+
+
+def log_and_write(message, info_file):
+    logger.info(message)
+    info_file.write(message + "\n")
 
 
 if __name__ == "__main__":
-
     logging.basicConfig(
         format='%(asctime)s | %(levelname)s | %(module)s | %(message)s',
         datefmt="%Y-%m-%d %H:%M:%S",
         level=logging.INFO)
     logger.info("Running %s" % " ".join(sys.argv))
-    logger.info("")
 
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--corpuspath")
+    parser.add_argument("--outdir")
+    args = vars(parser.parse_args())
+
+    main(corpus_path=args["corpuspath"], output_dir=args["outdir"])
 
     logger.info("Done!")
