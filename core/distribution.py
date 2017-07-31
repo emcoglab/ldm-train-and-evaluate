935bbce74ba9d40844007712734c511afb2794a3
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-31 13:10:12 +0100

    Optional logging

--- core/distribution.py
+++ core/distribution.py
@@ -1,13 +1,21 @@
+import logging
+
 import nltk
 
 
-def accumulate_frequency_distribution_from_file(filename, batch_size=1_000_000):
+logger = logging.getLogger(__name__)
+
+
+def freq_dist_from_file(filename, batch_size=1_000_000, verbose=False):
     """
-    Produces a nltk.probability.FreqDist from a corpus without loading the entire thing into RAM at one time.
+    Produces a nltk.probability.FreqDist from a corpus in batches, without loading the entire thing into RAM at one
+    time.
     :param filename:
     The filename of the corpus file. Should be a single text file with individual tokens on each line.
     :param batch_size:
     The number of tokens to load into RAM at one time.  Increasing will speed up function, but require more memory.
+    :param verbose:
+    Logging toggle
     :return freq_dist:
     A nltk.probability.FreqDist from the corpus.
     """
@@ -18,6 +26,8 @@ def accumulate_frequency_distribution_from_file(filename, batch_size=1_000_000):
     with open(filename, mode="r", encoding="utf-8") as corpus_file:
 
         # counters
+        batch_count       = 0
+        token_count       = 0
         tokens_this_batch = 0
 
         # Add tokens to the batch
@@ -30,6 +40,11 @@ def accumulate_frequency_distribution_from_file(filename, batch_size=1_000_000):
                 # Dump the batch into the FreqDist
                 freq_dist += nltk.probability.FreqDist(batch)
 
+                batch_count += 1
+                token_count += tokens_this_batch
+                if verbose:
+                    logger.info(f"{batch_count:,} batches processed, {token_count:,} tokens total")
+
                 # Empty the batch
                 batch = []
                 tokens_this_batch = 0
@@ -38,3 +53,4 @@ def accumulate_frequency_distribution_from_file(filename, batch_size=1_000_000):
         freq_dist += nltk.probability.FreqDist(batch)
 
     return freq_dist
+
