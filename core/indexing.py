219446aefc8e2ac4f58d20c0db9000af61a7ea18
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-31 16:27:23 +0100

    TokenIndexDictionary class.

--- core/indexing.py
+++ core/indexing.py
@@ -1,16 +1,57 @@
-def index_dictionary(freq_dist):
-    """
-    Builds dictionaries of word indices (starting at 0).  Indices will be sorted by token frequency
-    :param freq_dist: A nltk.probability.FreqDist.
-    :return:
-    """
+import pickle
 
-    word2id = {}
-    current_id = 0
-    for token, _freq in freq_dist.most_common():
-        word2id[token] = current_id
-        current_id += 1
+import nltk
 
-    id2word = dict((v, k) for k, v in word2id.items())
 
-    return word2id, id2word
+class TokenIndexDictionary(object):
+
+    # A dictionary of token-keyed indices
+    token2id = None
+
+    # A dictionary of index-keyed tokens
+    id2token = None
+
+    def __init__(self, word2id, id2word):
+        """
+        Constructor.
+        :param word2id:
+        :param id2word:
+        """
+        self.token2id = word2id
+        self.id2token = id2word
+
+    def save(self, filename):
+        """
+        Saves an TokenIndexDictionary to a file
+        :param filename:
+        :return:
+        """
+        with open(filename, mode="wb") as file:
+            pickle.dump(self, file)
+
+    @staticmethod
+    def from_freqdist(freq_dist: nltk.probability.FreqDist):
+        """
+        Constructs an TokenIndexDictionary from a nltk.probability.FreqDist
+        :param freq_dist:
+        :return:
+        """
+        word2id = {}
+        current_id = 0
+        for token, _freq in freq_dist.most_common():
+            word2id[token] = current_id
+            current_id += 1
+
+        id2word = dict((v, k) for k, v in word2id.items())
+
+        return TokenIndexDictionary(word2id, id2word)
+
+    @staticmethod
+    def load(filename):
+        """
+        Loads an TokenIndexDictionary from a file.
+        :param filename:
+        :return:
+        """
+        with open(filename, mode="rb") as file:
+            return pickle.load(file)
--- scripts_count_model/1_word_indexing.py
+++ scripts_count_model/1_word_indexing.py
@@ -2,43 +2,48 @@ import logging
 import os
 import pickle
 import sys
-from collections import defaultdict
 
-import nltk
-import numpy as np
-import matplotlib.pyplot as pplot
-
-from ..core.indexing import index_dictionary
 from ..core.classes import CorpusMetaData
+from ..core.indexing import TokenIndexDictionary
 
 logger = logging.getLogger(__name__)
 
 
 def main():
+    fdist_metas = [
+        CorpusMetaData(
+            name="toy",
+            path="/Users/caiwingfield/corpora/toy-corpus/info/Frequency distribution toy.corpus.pickle"
+        ),
+        CorpusMetaData(
+            name="BBC",
+            path="/Users/caiwingfield/corpora/BBC/4.1 info/Frequency distribution BBC.corpus.pickle"
+        ),
+        CorpusMetaData(
+            name="BNC",
+            path="/Users/caiwingfield/corpora/BNC/2.1 info/Frequency distribution BNC.corpus.pickle"
+        ),
+        CorpusMetaData(
+            name="UKWAC",
+            path="/Users/caiwingfield/corpora/UKWAC/3.1 info/Frequency distribution UKWAC.corpus.pickle"
+        )]
 
     index_dir = "/Users/caiwingfield/vectors/indexes"
 
-    freq_dist_path = "/Users/caiwingfield/corpora/toy-corpus/info/Frequency distribution toy.corpus.pickle"
+    for fdist_meta in fdist_metas:
+        logger.info(f"Producing word index dictionaries for {fdist_meta.name} corpus")
 
-    corpus_name = "toy"
+        with open(fdist_meta.path, mode="rb") as freq_dist_file:
+            freq_dist = pickle.load(freq_dist_file)
 
-    with open(freq_dist_path, mode="rb") as freq_dist_file:
-        freq_dist = pickle.load(freq_dist_file)
+        token_index = TokenIndexDictionary.from_freqdist(freq_dist)
 
-    word2id, id2word = index_dictionary(freq_dist)
-
-    word2id_filename = os.path.join(index_dir, "{}_word2id.pickle".format(corpus_name))
-    id2word_filename = os.path.join(index_dir, "{}_id2word.pickle".format(corpus_name))
-
-    with open(word2id_filename, mode="wb") as word2id_file:
-        pickle.dump(word2id, word2id_file)
-    with open(id2word_filename, mode="wb") as id2word_file:
-        pickle.dump(id2word, id2word_file)
+        token_index.save(os.path.join(index_dir, f"{fdist_meta.name}.index"))
 
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S",
                         level=logging.INFO)
-    logger.info("running %s" % " ".join(sys.argv))
+    logger.info("Running %s" % " ".join(sys.argv))
     main()
     logger.info("Done!")
