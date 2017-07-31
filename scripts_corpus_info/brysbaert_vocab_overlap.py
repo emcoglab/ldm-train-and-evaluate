8c7da9c409526fd54654575e0275da2b71351354
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-07-31 15:10:47 +0100

    Brysbaert overlap for different high-pass filters

--- scripts_corpus_info/brysbaert_vocab_overlap.py
+++ scripts_corpus_info/brysbaert_vocab_overlap.py
@@ -1,28 +1,27 @@
 import argparse
 import logging
 import os
+import pickle
 import sys
 
 from ..core.classes import CorpusMetaData
 
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
-def main(corpus_path, output_dir):
+def main(corpus_path, output_dir, freq_dist_path=None):
 
     wordlist_meta = CorpusMetaData(
-        name="Brysbaert 1 word", path="/Users/caiwingfield/corpora/brysbaert40k/brysbaert1.wordlist")
+        name="Brysbaert 1 word",
+        path="/Users/caiwingfield/code/corpus_analysis/scripts_corpus_info/brysbaert1.wordlist")
 
     token_delimiter = "\n"
-
-    info_filename = "Brysbaert 1-word vocab overlap.info"
 
     logger.info(f"Loading wordlist from {wordlist_meta.path}")
     with open(wordlist_meta.path, mode="r", encoding="utf-8") as wordlist_file:
         vocab_wordlist = set(wordlist_file.read().split(token_delimiter))
-
-    logger.info(f"Wordlist has a vocab of size {len(vocab_wordlist):,}")
+        logger.info(f"Wordlist has a vocab of size {len(vocab_wordlist):,}")
 
     logger.info(f"Loading corpus documents from {corpus_path}")
     with open(corpus_path, mode="r", encoding="utf-8") as corpus_file:
@@ -32,22 +31,41 @@ def main(corpus_path, output_dir):
             if i % 10_000_000 == 0:
                 logger.info(f"\tRead {i:,} tokens")
 
-    with open(os.path.join(output_dir, info_filename), mode="w", encoding="utf-8") as info_file:
-        message = f"Corpus has a vocab of size {len(vocab_corpus):,}"
-        log_and_write(message, info_file)
+    # The filter freqs we'll use
+    filter_freqs = [0]
+    freq_dist = None
+    if freq_dist_path is not None:
+        filter_freqs.extend([1, 5, 10, 25, 50, 100])
+        with open(freq_dist_path, mode="rb") as freq_dist_file:
+            freq_dist = pickle.load(freq_dist_file)
 
-        overlap_vocab = set.intersection(vocab_corpus, vocab_wordlist)
+    for filter_freq in filter_freqs:
 
-        message = f"Overlap has a size of\t{len(overlap_vocab):,}"
-        log_and_write(message, info_file)
+        info_filename = f"Brysbaert 1-word vocab overlap (filter {filter_freq}).info"
 
-        missing_vocab = vocab_wordlist - vocab_corpus
+        # remove filtered terms from vocab
+        filtered_vocab = vocab_corpus
+        if filter_freq > 0:
+            for token, freq in freq_dist.most_common():
+                if freq <= filter_freq:
+                    filtered_vocab.discard(token)
 
-        message = f"Missing words: {len(missing_vocab)}"
-        log_and_write(message, info_file)
+        with open(os.path.join(output_dir, info_filename), mode="w", encoding="utf-8") as info_file:
+            message = f"Corpus (filter {filter_freq}) has a vocab of size {len(filtered_vocab):,}"
+            log_and_write(message, info_file)
 
-        message = f"{missing_vocab}"
-        log_and_write(message, info_file)
+            overlap_vocab = set.intersection(filtered_vocab, vocab_wordlist)
+
+            message = f"Overlap (filter {filter_freq}) has a size of\t{len(overlap_vocab):,}"
+            log_and_write(message, info_file)
+
+            missing_vocab = vocab_wordlist - filtered_vocab
+
+            message = f"Missing words (filter {filter_freq}): {len(missing_vocab):,}"
+            log_and_write(message, info_file)
+
+            message = f"{missing_vocab}"
+            log_and_write(message, info_file)
 
 
 def log_and_write(message, info_file):
@@ -65,8 +83,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--corpuspath")
     parser.add_argument("--outdir")
+    parser.add_argument("--freqdist", default=None)
     args = vars(parser.parse_args())
 
-    main(corpus_path=args["corpuspath"], output_dir=args["outdir"])
+    main(corpus_path=args["corpuspath"], output_dir=args["outdir"], freq_dist_path=args["freqdist"])
 
     logger.info("Done!")
