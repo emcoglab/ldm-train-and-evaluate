e97e92658daadf08a9e5c73ef7f191c176138f34
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-08-01 12:39:19 +0100

    Run on all corpora.

--- scripts_models_predict/1_skip_gram.py
+++ scripts_models_predict/1_skip_gram.py
@@ -4,37 +4,62 @@ import os
 
 import gensim
 
-from ..core.corpus import BatchedCorpus
+from ..core.corpus import BatchedCorpus, CorpusMetaData
 
 logger = logging.getLogger(__name__)
 
 
 def main():
 
-    model_save_filename = "/Users/caiwingfield/vectors/skip-gram/BNC.skipgram"
+    metas = [
+        dict(
+            corpus=CorpusMetaData(
+                name="BBC",
+                path="/Users/caiwingfield/corpora/BBC/4 Tokenised/BBC.corpus"),
+            weights_save="/Users/caiwingfield/vectors/skip-gram/BBC.skipgram"),
+        dict(
+            corpus=CorpusMetaData(
+                name="BNC",
+                path="/Users/caiwingfield/corpora/BNC/2 Tokenised/BNC.corpus"),
+            weights_save="/Users/caiwingfield/vectors/skip-gram/BNC.skipgram"),
+        dict(
+            corpus=CorpusMetaData(
+                name="UKWAC",
+                path="/Users/caiwingfield/corpora/UKWAC/3 Tokenised/UKWAC.corpus"),
+            weights_save="/Users/caiwingfield/vectors/skip-gram/UKWAC.skipgram")
+    ]
 
-    if not os.path.isfile(model_save_filename):
+    for meta in metas:
 
-        embedding_dims      = 100
-        window_radius       = 5
-        ignorable_frequency = 1
+        logger.info(f"Running on corpus {meta['corpus'].name}")
 
-        corpus = BatchedCorpus(filename="/Users/caiwingfield/corpora/BNC/2 Tokenised/BNC.corpus", batch_size=10)
+        if not os.path.isfile(meta['weights_save']):
 
-        model = gensim.models.Word2Vec(
-            # TODO: does using disjoing "sentences" here lead to unpleasant edge effects?
-            sentences=corpus,
-            size=embedding_dims,
-            window=window_radius,
-            min_count=ignorable_frequency,
-            workers=4)
+            logger.info("Training Skip-gram")
 
-        model.save(model_save_filename)
+            embedding_dims      = 100
+            window_radius       = 5
+            ignorable_frequency = 1
 
-    else:
-        model = gensim.models.Word2Vec.load('/tmp/mymodel')
+            corpus = BatchedCorpus(filename=meta['corpus'].path, batch_size=10)
 
-    logger.info(model.most_similar(positive=['woman', 'king'], negative=['man'], topn=4))
+            # TODO: is this actually skip-gram or cbow?
+            model = gensim.models.Word2Vec(
+                # TODO: does using disjoing "sentences" here lead to unpleasant edge effects?
+                sentences=corpus,
+                size=embedding_dims,
+                window=window_radius,
+                min_count=ignorable_frequency,
+                workers=4)
+
+            model.save(meta['weights_save'])
+
+        else:
+            logger.info("Loading pre-trained model")
+            model = gensim.models.Word2Vec.load(meta['weights_save'])
+
+        logger.info(f"For corpus {meta['corpus'].name}:")
+        logger.info(model.most_similar(positive=['woman', 'king'], negative=['man'], topn=4))
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s',
