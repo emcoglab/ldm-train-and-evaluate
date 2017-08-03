9c41887e9392f0431e548007a1166218cce664bc
Cai <mail@caiwingfield.com>
2017-08-03 12:33:53 +0100

    Only compute exactly-n cooccurrences, not summed.  It'll be much faster!

--- scripts_models/2_raw_cooccurrence_counts.py
+++ scripts_models/2_raw_cooccurrence_counts.py
@@ -36,16 +36,11 @@ def main():
 
         for radius in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
 
-            # We will load in the full window and count left and right cooccurences separately
-            # The size of the symmetric window is twice the radius, plus 1 (centre)
-            diameter = 2 * radius + 1
-
-            fname_l = os.path.join(out_dir, f"{meta.name}_r={radius}_left.cooccur")
-            fname_r = os.path.join(out_dir, f"{meta.name}_r={radius}_right.cooccur")
-            fname_b = os.path.join(out_dir, f"{meta.name}_r={radius}_both.cooccur")
+            filename_l = os.path.join(out_dir, f"{meta.name}_r={radius}_left.cooccur")
+            filename_r = os.path.join(out_dir, f"{meta.name}_r={radius}_right.cooccur")
 
             # Skip ones which are already done
-            if os.path.isfile(fname_b):
+            if os.path.isfile(filename_r):
                 logger.info(f"Skipping {meta.name} corpus, radius {radius}")
             else:
                 logger.info(f"Working on {meta.name} corpus, radius {radius}")
@@ -54,13 +49,16 @@ def main():
 
                 vocab_size = len(token_indices)
 
-                # Indices
-                lh_context_is = range(0, radius)
-                target_i = radius
-                rh_context_is = range(radius + 1, diameter)
+                # Initialise cooccurrence matrices
 
-                # first coordinate points to target word
-                # second coordinate points to context word
+                # We will store left- and right-cooccurrences separately.
+                # At this stage we define radius-n cooccurrence to be words which are /exactly/ n words apart, rather
+                # than /up-to/ n words apart.
+                # This will greatly speed up computation, and we can sum the values later much faster to get the
+                # standard "summed" n-gram counts.
+
+                # First coordinate points to target word
+                # Second coordinate points to context word
                 cooccur_l = sps.lil_matrix((vocab_size, vocab_size))
                 cooccur_r = sps.lil_matrix((vocab_size, vocab_size))
 
@@ -68,38 +66,29 @@ def main():
                 window_count = 0
                 for window in WindowedCorpus(meta, radius):
 
-                        # The window is now full
-                        target_token = window[target_i]
-                        target_id = token_indices.token2id[target_token]
+                    # The target token is the one in the middle, whose index is the radius of the window
+                    target_token = window[radius]
+                    target_id = token_indices.token2id[target_token]
 
-                        # Count lh occurrences
-                        for i in lh_context_is:
-                            context_token = window[i]
-                            context_id = token_indices.token2id[context_token]
-                            cooccur_l[target_id, context_id] += 1
+                    # Count lh occurrences
+                    context_token = window[0]
+                    context_id = token_indices.token2id[context_token]
+                    cooccur_l[target_id, context_id] += 1
 
-                        # Count rh occurrences
-                        for i in rh_context_is:
-                            context_token = window[i]
-                            context_id = token_indices.token2id[context_token]
-                            cooccur_r[target_id, context_id] += 1
+                    # Count rh occurrences
+                    context_token = window[-1]
+                    context_id = token_indices.token2id[context_token]
+                    cooccur_r[target_id, context_id] += 1
 
-                        window_count += 1
-                        if window_count % 1_000_000 == 0:
-                            logger.info(f"\t{window_count:,} tokens processed")
+                    window_count += 1
+                    if window_count % 1_000_000 == 0:
+                        logger.info(f"\t{window_count:,} tokens processed")
 
                 logger.info("Saving co-occurrence matrices")
-
-                cooccur_lr = sps.lil_matrix((vocab_size, vocab_size))
-                cooccur_lr += cooccur_l
-                cooccur_lr += cooccur_r
-
-                with open(fname_l, mode="wb") as cooccur_file:
+                with open(filename_l, mode="wb") as cooccur_file:
                     pickle.dump(cooccur_l, cooccur_file)
-                with open(fname_r, mode="wb") as cooccur_file:
+                with open(filename_r, mode="wb") as cooccur_file:
                     pickle.dump(cooccur_r, cooccur_file)
-                with open(fname_b, mode="wb") as cooccur_file:
-                    pickle.dump(cooccur_lr, cooccur_file)
 
 
 if __name__ == "__main__":
