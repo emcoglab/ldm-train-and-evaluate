617c01726490c7dc847be32de9d7fa9ec0175e28
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-10-13 13:52:00 +0100

    Line numbers in logs.

--- core/utils/logging.py
+++ core/utils/logging.py
@@ -1,2 +1,2 @@
-log_message = '%(asctime)s | %(levelname)s | %(module)s | %(message)s'
+log_message = '%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | \t%(message)s'
 date_format = "%Y-%m-%d %H:%M:%S"
