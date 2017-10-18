3927367fdeeb95950554f4525405154cceb3691e
Cai <mail@caiwingfield.com>
2017-10-18 13:20:22 +0100

    WIP plot one model.

--- scripts_figures/1_synonym.py
+++ scripts_figures/1_synonym.py
@@ -25,7 +25,7 @@ import numpy
 import pandas
 import seaborn
 
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as pplot
 
 from ..core.utils.logging import log_message, date_format
 from ..core.utils.maths import DistanceType
@@ -45,27 +45,45 @@ def main():
 
     df = ensure_column_safety(df)
 
-    df["model_name"] = df.apply(lambda r: f"{r['corpus']} {r['distance']} {r['model']} {r['embedding_size']}", axis=1)
+    df["model_name"] = df.apply(lambda r:
+                                f"{r['corpus']} {r['distance']} {r['model']} {r['embedding_size']}"
+                                if r['embedding_size'] != numpy.nan
+                                else f"{r['corpus']} {r['distance']} {r['model']}",
+                                axis=1)
 
     distance = DistanceType.cosine.name
     corpus = "BNC"
-    model= "CBOW"
-    embedding_size = numpy.nan
+    model = "CBOW"
+    embedding_size = 500
     test_name = "TOEFL"
 
-    fdf = df.query(
-        f"corpus == '{corpus}' & "
-        f"distance == '{distance}' & "
-        # f"model == '{model}' & "
-        # f"embedding_size == {embedding_size} & "
-        f"test_name == '{test_name}'"
-    )
+    fdf: pandas.DataFrame = df.copy()
+    fdf = fdf[fdf["corpus"] == corpus]
+    fdf = fdf[fdf["distance"] == distance]
+    fdf = fdf[fdf["model"] == model]
+    fdf = fdf[fdf["embedding_size"] == embedding_size]
+    fdf = fdf[fdf["test_name"] == test_name]
 
-    fdf = fdf.sort_values(by=["model_name", "radius"]).reindex()
+    fdf = fdf.sort_values(by=["model_name", "radius"])
+    fdf = fdf.reset_index(drop=True)
 
-    fdf = fdf[["model_name", "radius", "score"]]
+    fdf = fdf[[
+        "model_name",
+        "radius",
+        "score"]]
 
-    seaborn.factorplot(data=fdf, x='radius', y="score", hue="model_name")
+    # plot = seaborn.factorplot(data=fdf, x='radius', y="score")
+    #
+    # plot.savefig(os.path.join(figures_dir, "test.png"))
+
+    ax = fdf.plot(x="radius", y="score", ylim=[0, 1])
+
+    # Format as percent
+    vals = ax.get_yticks()
+    ax.set_yticklabels(['{:3.2f}%'.format(x * 100) for x in vals])
+
+    # Save figure
+    pplot.savefig(os.path.join(figures_dir, "test.png"))
 
     pass
 
@@ -77,6 +95,9 @@ def load_data() -> pandas.DataFrame:
     results_dir = Preferences.synonym_results_dir
     separator = ","
 
+    def percent_to_float(percent: str) -> float:
+        return float(percent.strip("%"))/100
+
     header_filename = os.path.join(results_dir, " header.csv")
     data_filenames = glob(os.path.join(results_dir, "*.csv"))
     data_filenames.remove(header_filename)
@@ -87,7 +108,7 @@ def load_data() -> pandas.DataFrame:
     data = pandas.DataFrame(columns=column_names)
 
     for data_filename in data_filenames:
-        partial_df = pandas.read_csv(data_filename, sep=separator, names=column_names)
+        partial_df = pandas.read_csv(data_filename, sep=separator, names=column_names, converters={'Score': percent_to_float})
         data = data.append(partial_df, ignore_index=True)
 
     return data
