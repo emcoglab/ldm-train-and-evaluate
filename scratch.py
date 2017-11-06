080bba66e0f697877428ade4c9455b6924693fc9
Cai <mail@caiwingfield.com>
2017-11-06 17:01:58 +0000

    Quick script to convert from report card csvs to dataframe csvs.

--- scratch.py
+++ scratch.py
@@ -1,13 +1,54 @@
-from pandas import DataFrame
+import os
+import glob
+import pandas
 
-df = DataFrame({'val':  [ 2,    3,    5,    7  ],
-                'foo':  ['f1', 'f2', 'f3', 'f4']},
-               index=['n1', 'n2', 'n3', 'n4'])
+from .core.evaluation.synonym import SynonymResults
+from .core.evaluation.association import AssociationResults
+from .preferences.preferences import Preferences
 
-v = df.val.values[:, None] * df.val.values
 
-x = df.foo.values[:, None] + df.foo.values
+def main():
 
-d = DataFrame(x, df.index, df.index)
+    convert_synonyms()
+    convert_associations()
 
-pass
+
+def convert_synonyms():
+    results_dir = Preferences.synonym_results_dir
+    separator = ","
+    header_filename = os.path.join(results_dir, " header.csv")
+    data_filenames = glob.glob(os.path.join(results_dir, "*.csv"))
+    data_filenames.remove(header_filename)
+    with open(os.path.join(results_dir, " header.csv"), mode="r", encoding="utf-8") as header_file:
+        column_names = header_file.read().strip().split(separator)
+    results_df = pandas.DataFrame(columns=column_names)
+    for data_filename in data_filenames:
+        partial_df = pandas.read_csv(data_filename, sep=separator, names=column_names)
+        results_df = results_df.append(partial_df, ignore_index=True)
+    new_results = SynonymResults()
+    new_results.data = results_df
+    assert set(new_results.column_names) == set(results_df.columns.values)
+    new_results.save()
+
+
+def convert_associations():
+    results_dir = Preferences.association_results_dir
+    separator = ","
+    header_filename = os.path.join(results_dir, " header.csv")
+    data_filenames = glob.glob(os.path.join(results_dir, "*.csv"))
+    data_filenames.remove(header_filename)
+    with open(os.path.join(results_dir, " header.csv"), mode="r", encoding="utf-8") as header_file:
+        column_names = header_file.read().strip().split(separator)
+    results_df = pandas.DataFrame(columns=column_names)
+    for data_filename in data_filenames:
+        partial_df = pandas.read_csv(data_filename, sep=separator, names=column_names)
+        results_df = results_df.append(partial_df, ignore_index=True)
+    new_results = AssociationResults()
+    new_results.data = results_df
+    assert set(new_results.column_names) == set(results_df.columns.values)
+    new_results.save()
+
+
+if __name__ == '__main__':
+
+    main()
