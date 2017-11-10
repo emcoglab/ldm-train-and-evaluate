7753dc08e1e3e9dddc8608045cd1533dd3190b62
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-11-10 11:26:33 +0000

    Prepare to refactor comparison functions by paramater.

--- scripts_output/5_priming_parameter_value_comparisons.py
+++ scripts_output/5_priming_parameter_value_comparisons.py
@@ -61,26 +61,29 @@ def main():
 
     results_df = load_data()
 
+    compare_using_radius(results_df)
+    compare_using_embedding_size(results_df)
+
     parameter_value_comparison(results_df)
 
 
-def parameter_value_comparison(results_df: DataFrame):
-    summary_dir = Preferences.summary_dir
+def compare_using_radius(results_df: DataFrame):
+
+    param_name = "Window radius"
+
     figures_dir = os.path.join(figures_base_dir, "parameter comparisons")
 
     results_all_dvs = []
 
-    for dv_name in DV_NAMES:
+    for dv in DV_NAMES:
 
-        this_dv_df = results_df[results_df["Dependent variable"] == dv_name].copy()
-
-        # RADIUS
+        this_dv_df = results_df[results_df["Dependent variable"] == dv].copy()
 
         # Distributions of winning-radius-vs-next-best bfs
-        radius_dist = defaultdict(list)
+        bf_distributions = defaultdict(list)
 
         # model name, not including radius
-        this_dv_df["model_name"] = this_dv_df.apply(
+        this_dv_df["Model name"] = this_dv_df.apply(
             lambda r:
             f"{r['Model type']} {r['Embedding size']:.0f} {r['Distance type']} {r['Corpus']}"
             if r['Embedding size'] is not None and not numpy.isnan(r['Embedding size'])
@@ -93,123 +96,132 @@ def parameter_value_comparison(results_df: DataFrame):
             all_radii_df = all_radii_df.sort_values("B10 approx", ascending=False).reset_index(drop=True)
 
             # add bfs for winning radius to results
-            radius_dist[all_radii_df["Window radius"][0]].append(all_radii_df["B10 approx"][0]/all_radii_df["B10 approx"][1])
+            bf_distributions[all_radii_df[param_name][0]].append(
+                all_radii_df["B10 approx"][0] / all_radii_df["B10 approx"][1])
 
-        radius_results_this_dv = []
+        results_this_dv = []
 
-        for radius in Preferences.window_radii:
+        param_values = Preferences.window_radii
+
+        for param_value in param_values:
 
             # Add to results
 
-            radius_results_this_dv.append(
-                # value  number of wins
-                [radius, len(radius_dist[radius])]
+            results_this_dv.append(
+                # value       number of wins
+                [param_value, len(bf_distributions[param_value])]
             )
 
             # Make figure
 
-            if len(radius_dist[radius]) > 1:
-
+            if len(bf_distributions[param_value]) > 1:
                 seaborn.set_context(context="paper", font_scale=1)
-                plot = seaborn.distplot(radius_dist[radius], kde=False, color="b")
+                plot = seaborn.distplot(bf_distributions[param_value], kde=False, color="b")
 
                 plot.axes.set_xlim(1, None)
 
                 plot.set_xlabel("BF")
-                plot.set_title(f"BFs for winning RADIUS={radius} versus next competitor for {dv_name}")
+                plot.set_title(f"BFs for winning {param_name.lower()}={param_value} versus next competitor for {dv}")
 
-                plot.figure.savefig(os.path.join(figures_dir, f"priming RADIUS={radius} bf dist {dv_name}.png"), dpi=300)
+                plot.figure.savefig(os.path.join(figures_dir, f"priming {param_name}={param_value} bf dist {dv}.png"),
+                                    dpi=300)
 
                 pyplot.close(plot.figure)
 
-        for result in radius_results_this_dv:
-            results_all_dvs.append([dv_name, "Radius"] + result)
+        for result in results_this_dv:
+            results_all_dvs.append([dv, param_name] + result)
 
-        results_this_dv_df = DataFrame(radius_results_this_dv, columns=["Radius", "Number of times winner"])
+        results_this_dv_df = DataFrame(results_this_dv, columns=[param_name, "Number of times winner"])
 
         seaborn.set_context(context="paper", font_scale=1)
-        plot = seaborn.barplot(x=results_this_dv_df["Radius"], y=results_this_dv_df["Number of times winner"])
+        plot = seaborn.barplot(x=results_this_dv_df[param_name], y=results_this_dv_df["Number of times winner"])
 
-        plot.set_xlabel("Radius")
-        plot.set_title(f"Number of times each radius is the best for {dv_name}")
+        plot.set_xlabel(param_name)
+        plot.set_title(f"Number of times each {param_name.lower()} is the best for {dv}")
 
-        plot.figure.savefig(os.path.join(figures_dir, f"priming RADIUS bf dist {dv_name}.png"), dpi=300)
+        plot.figure.savefig(os.path.join(figures_dir, f"priming {param_name.lower()} bf dist {dv}.png"), dpi=300)
 
         pyplot.close(plot.figure)
 
-        # END RADIUS
 
-        # EMBEDDING SIZE
+def compare_using_embedding_size(results_df: DataFrame):
+
+    param_name = "Embedding size"
+
+    figures_dir = os.path.join(figures_base_dir, "parameter comparisons")
+
+    results_all_dvs = []
+
+    for dv in DV_NAMES:
+
+        this_dv_df = results_df[results_df["Dependent variable"] == dv].copy()
 
         # Distributions of winning-radius-vs-next-best bfs
-        embedding_size_dist = defaultdict(list)
+        bf_distributions = defaultdict(list)
+
+        # Only predict models have embedding sizes, and that's all we're interested in.
+        this_dv_df = this_dv_df[this_dv_df["Model category"] == "Predict"]
 
         # model name, not including embedding size
-        this_dv_df = results_df[results_df["Dependent variable"] == dv_name].copy()
         this_dv_df["Model name"] = this_dv_df.apply(
             lambda r:
             f"{r['Model type']} r={r['Window radius']} {r['Distance type']} {r['Corpus']}",
             axis=1
         )
 
-        # don't care about models without embedding sizes
-        # TODO: store count/predict in regression results
-        this_dv_df = this_dv_df[pandas.notnull(this_dv_df["Embedding size"])]
-
-        # make embedding sizes ints
-        # TODO: fix this at source
-        this_dv_df["embedding_size"] = this_dv_df.apply(lambda r: int(r['Embedding size']), axis=1)
-
         for model_name in this_dv_df["Model name"].unique():
             all_embed_df = this_dv_df[this_dv_df["Model name"] == model_name]
             all_embed_df = all_embed_df.sort_values("b10_approx", ascending=False).reset_index(drop=True)
 
             # add bfs for winning radius to results
-            embedding_size_dist[all_embed_df["Embedding size"][0]].append(all_embed_df["B10 approx"][0]/all_embed_df["B10 approx"][1])
+            bf_distributions[all_embed_df[param_name][0]].append(all_embed_df["B10 approx"][0]/all_embed_df["B10 approx"][1])
 
-        embedding_results_this_dv = []
+        results_this_dv = []
 
-        for embedding_size in Preferences.predict_embedding_sizes:
+        param_values = Preferences.predict_embedding_sizes
+
+        for param_value in param_values:
 
             # Add to results
 
-            embedding_results_this_dv.append(
-                # value          number of wins
-                [embedding_size, len(embedding_size_dist[embedding_size])]
+            results_this_dv.append(
+                # value       number of wins
+                [param_value, len(bf_distributions[param_value])]
             )
 
             # Make figure
 
-            if len(embedding_size_dist[embedding_size]) > 1:
+            if len(bf_distributions[param_value]) > 1:
 
                 seaborn.set_context(context="paper", font_scale=1)
-                plot = seaborn.distplot(embedding_size_dist[embedding_size], kde=False, color="b")
+                plot = seaborn.distplot(bf_distributions[param_value], kde=False, color="b")
 
                 plot.axes.set_xlim(1, None)
 
                 plot.set_xlabel("BF")
-                plot.set_title(f"BFs for winning EMBED={embedding_size} versus next competitor for {dv_name}")
+                plot.set_title(f"BFs for winning {param_name.lower()}={param_value} versus next competitor for {dv}")
 
-                plot.figure.savefig(os.path.join(figures_dir, f"priming EMBED={embedding_size} bf dist {dv_name}.png"), dpi=300)
+                plot.figure.savefig(os.path.join(figures_dir, f"priming {param_name}={param_value} bf dist {dv}.png"), dpi=300)
 
                 pyplot.close(plot.figure)
 
-        for result in embedding_results_this_dv:
-            results_all_dvs.append([dv_name, "Embedding size"] + result)
+        for result in results_this_dv:
+            results_all_dvs.append([dv, param_name] + result)
 
-        results_this_dv_df = DataFrame(embedding_results_this_dv, columns=["Embedding size", "Number of times winner"])
+        results_this_dv_df = DataFrame(results_this_dv, columns=[param_name, "Number of times winner"])
 
         seaborn.set_context(context="paper", font_scale=1)
-        plot = seaborn.barplot(x=results_this_dv_df["Embedding size"], y=results_this_dv_df["Number of times winner"])
+        plot = seaborn.barplot(x=results_this_dv_df[param_name], y=results_this_dv_df["Number of times winner"])
 
-        plot.set_xlabel("Embedding size")
-        plot.set_title(f"Number of times each embedding size is the best for {dv_name}")
+        plot.set_xlabel(param_name)
+        plot.set_title(f"Number of times each {param_name.lower()} is the best for {dv}")
 
-        plot.figure.savefig(os.path.join(figures_dir, f"priming EMBED bf dist {dv_name}.png"), dpi=300)
+        plot.figure.savefig(os.path.join(figures_dir, f"priming {param_name.lower()} bf dist {dv}.png"), dpi=300)
 
         pyplot.close(plot.figure)
 
-        # END EMBEDDING SIZE
+
+def parameter_value_comparison(results_df: DataFrame):
 
     all_results_df = DataFrame(results_all_dvs, columns=["DV name", "Parameter", "Value", "Number of times winner"])
     all_results_df.to_csv(os.path.join(summary_dir, "priming parameter wins.csv"), index=False)
@@ -253,7 +265,9 @@ def load_data() -> DataFrame:
     separator = ","
 
     with open(os.path.join(results_dir, "regression.csv"), mode="r", encoding="utf-8") as regression_file:
-        regression_df = pandas.read_csv(regression_file, sep=separator, header=0)
+        regression_df = pandas.read_csv(regression_file, sep=separator, header=0, dtype={"Embedding size": int})
+
+    regression_df["Model category"] = regression_df.apply(lambda r: "Count" if pandas.isnull(regression_df["Embedding size"]) else "Predict", axis=1)
 
     return regression_df
 
