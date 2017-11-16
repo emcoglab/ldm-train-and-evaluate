ff0e1a61cf55a2c798771f5e678c9dfe153513d9
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2017-11-16 15:04:48 +0000

    Bayesian parameter comparisons for similarity judgements.

--- scripts_output/0_crit_path.sh
+++ scripts_output/0_crit_path.sh
@@ -6,4 +6,5 @@ python -m corpus_analysis.scripts_output.1_synonym
 python -m corpus_analysis.scripts_output.2_similarity
 python -m corpus_analysis.scripts_output.3_priming
 python -m corpus_analysis.scripts_output.4_norms
-python -m corpus_analysis.scripts_output.5_priming_param_comparison
+python -m corpus_analysis.scripts_output.5_similarity_param_comparison
+python -m corpus_analysis.scripts_output.6_priming_param_comparison
