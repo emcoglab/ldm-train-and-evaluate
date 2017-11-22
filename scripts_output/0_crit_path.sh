f922d4597c204e5b4bae491bbab75b2f296a9c8b
Cai <mail@caiwingfield.com>
2017-11-22 12:35:54 +0000

    Rename files after having deleted separate norm figure scripts.

--- scripts_output/0_crit_path.sh
+++ scripts_output/0_crit_path.sh
@@ -5,8 +5,6 @@ cd /Users/caiwingfield/code/
 python -m corpus_analysis.scripts_output.1_synonym
 python -m corpus_analysis.scripts_output.2_similarity
 python -m corpus_analysis.scripts_output.3_priming
-python -m corpus_analysis.scripts_output.4_norms
-python -m corpus_analysis.scripts_output.5_synonym_param_comparison
-python -m corpus_analysis.scripts_output.6_similarity_param_comparison
-python -m corpus_analysis.scripts_output.7_priming_param_comparison
-python -m corpus_analysis.scripts_output.8_norms_param_comparison
+python -m corpus_analysis.scripts_output.4_synonym_param_comparison
+python -m corpus_analysis.scripts_output.5_similarity_param_comparison
+python -m corpus_analysis.scripts_output.6_priming_param_comparison
