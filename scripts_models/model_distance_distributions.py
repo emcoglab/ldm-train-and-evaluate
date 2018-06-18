937a3ea94ecb6afd361673227022ff5b03bae228
Cai <c.wingfield@lancaster.ac.uk>
2018-06-18 10:17:35 +0100

    Needs to be wrapped in a main() function for entry_debug to work.

--- scripts_models/model_distance_distributions.py
+++ scripts_models/model_distance_distributions.py
@@ -20,64 +20,73 @@ import numpy
 from matplotlib import pyplot
 from os import path
 
-from core.corpus.indexing import TokenIndexDictionary, FreqDist
-from core.model.count import PPMIModel
-from core.utils.maths import DistanceType
-from preferences.preferences import Preferences
+from ..core.corpus.indexing import TokenIndexDictionary, FreqDist
+from ..core.model.count import PPMIModel
+from ..core.utils.maths import DistanceType
+from ..preferences.preferences import Preferences
 
 logger = logging.getLogger(__name__)
 
-for corpus_metadata in Preferences.source_corpus_metas:
 
-    token_index = TokenIndexDictionary.load(corpus_metadata.index_path)
-    freq_dist = FreqDist.load(corpus_metadata.freq_dist_path)
+def main():
+    for corpus_metadata in Preferences.source_corpus_metas:
 
-    for window_radius in Preferences.window_radii:
+        token_index = TokenIndexDictionary.load(corpus_metadata.index_path)
+        freq_dist = FreqDist.load(corpus_metadata.freq_dist_path)
 
-        # COUNT MODELS
+        distance_type = DistanceType.cosine
 
-        count_models = [
-            # LogCoOccurrenceCountModel(corpus_metadata, window_radius, token_index),
-            # ConditionalProbabilityModel(corpus_metadata, window_radius, token_index, freq_dist),
-            # ProbabilityRatioModel(corpus_metadata, window_radius, token_index, freq_dist),
-            PPMIModel(corpus_metadata, window_radius, token_index, freq_dist)
-        ]
+        for window_radius in Preferences.window_radii:
 
-        for model in count_models:
-            model.train(memory_map=True)
+            logger.info(f"Window radius {window_radius}")
 
-            distance_type = DistanceType.cosine
+            # COUNT MODELS
 
-            # Aggregate distances in histogram
+            count_models = [
+                # LogCoOccurrenceCountModel(corpus_metadata, window_radius, token_index),
+                # ConditionalProbabilityModel(corpus_metadata, window_radius, token_index, freq_dist),
+                # ProbabilityRatioModel(corpus_metadata, window_radius, token_index, freq_dist),
+                PPMIModel(corpus_metadata, window_radius, token_index, freq_dist)
+            ]
 
-            # Define histogram parameters
-            data_min = 0  # min possible distance
-            data_max = 2  # max possible distance
-            n_bins = 50  # number of bins
+            for model in count_models:
+                logger.info(f"Training model {model.name}")
+                model.train(memory_map=True)
 
-            bins = numpy.linspace(data_min, data_max, n_bins)
+                # Aggregate distances in histogram
 
-            overall_histogram = numpy.zeros(n_bins - 1, dtype='int32')
+                # Define histogram parameters
+                data_min = 0  # min possible distance
+                data_max = 2  # max possible distance
+                n_bins = 50  # number of bins
 
-            for word_1 in model.token_indices.tokens:
-                distances_this_word = []
-                for word_2 in model.token_indices.tokens:
-                    if word_1 == word_2:
-                        continue
-                    distances_this_word.append(model.distance_between(word_1, word_2, distance_type))
+                bins = numpy.linspace(data_min, data_max, n_bins)
+
+                overall_histogram = numpy.zeros(n_bins - 1, dtype='int32')
+
+                n_tokens = len(model.token_indices.tokens)
 
                 # Accumulate histogram
-                histogram_this_word, _ = numpy.histogram(distances_this_word, bins)
-                overall_histogram += histogram_this_word
+                for i1, word_1 in enumerate(model.token_indices.tokens):
+                    logger.info(f"Working on word {i1+1} of {n_tokens}")
+                    distances_this_word = [model.distance_between(word_1, word_2, distance_type)
+                                           for word_2 in model.token_indices.tokens
+                                           if not word_1 == word_2]
+                    histogram_this_word, _ = numpy.histogram(distances_this_word, bins)
+                    overall_histogram += histogram_this_word
 
-            # Save histogram
-            bar_width = 1 * (bins[1] - bins[0])
-            bar_centres = (bins[:-1] + bins[1:]) / 2
-            f, a = pyplot.subplots()
-            a.bar(bar_centres, overall_histogram, align="center", width=bar_width)
-            fig_name = f"distance distribution for {model.name} {distance_type.name}.png"
-            f.savefig(path.join(Preferences.figures_dir, fig_name))
+                # Save histogram
+                bar_width = 1 * (bins[1] - bins[0])
+                bar_centres = (bins[:-1] + bins[1:]) / 2
+                f, a = pyplot.subplots()
+                a.bar(bar_centres, overall_histogram, align="center", width=bar_width)
+                fig_name = f"distance distribution for {model.name} {distance_type.name}.png"
+                f.savefig(path.join(Preferences.figures_dir, fig_name))
 
-            # release memory
-            model.untrain()
-        del count_models
+                # release memory
+                model.untrain()
+            del count_models
+
+
+if __name__ == '__main__':
+    main()
